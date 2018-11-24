b6b3ae30b280975cf7935a0c351bd7ee788679d2
piyo7 <piyo7@users.noreply.github.com>
2018-11-24 21:47:52 +0900

    deprecate my Using

--- src/main/g8/src/main/scala/Using.scala
+++ src/main/g8/src/main/scala/Using.scala
@@ -2,6 +2,7 @@ import java.io.Writer
 
 import scala.io.Source
 
+@deprecated("repalce it with scala.util.Using", "Scala 2.13")
 object Using {
   def apply[A, B](resource: A)(process: A => B)(implicit closer: Closer[A]): B =
     try {
